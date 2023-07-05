04d510913045d99aa4ebda28ab39189d453fd30d
alek <wait82985@gmail.com>
2023-07-05 23:27:18 +0600

    problem solved

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,29 +1,40 @@
 public class Solution {
     public int findMaxForm(String[] strs, int m, int n) {
-
-        return dfs(strs, m, n, 0);
+        int mCount = 0;
+        int nCount = 0;
+        int[][] counts = new int[strs.length][2];
+        for (int i = 0; i < strs.length; i++) {
+            for (int j = 0; j < strs[i].length(); j++) {
+                if (strs[i].charAt(j) == '0') {
+                    mCount++;
+                } else nCount++;
+            }
+            counts[i][0] = mCount;
+            counts[i][1] = nCount;
+            mCount = 0;
+            nCount = 0;
+        }
+        Integer[][][] memo  = new Integer[101][101][strs.length + 1];
+        return dfs(counts, m, n, 0, memo);
     }
 
-    private int dfs(String[] strs, int m, int n, int i) {
-        if (i == strs.length) {
+    private int dfs(int[][] counts, int m, int n, int i, Integer[][][] memo) {
+        if (i == counts.length) {
             return 0;
+        }
+        if(memo[m][n][i] != null){
+            return memo[m][n][i];
         }
 
         int result = 0;
-        String str = strs[i];
-        int mCount = 0;
-        int nCount = 0;
-        for (int j = 0; j < str.length(); j++) {
-            if (str.charAt(j) == '0') {
-                mCount++;
-            } else nCount++;
-        }
+
         int take = 0;
-        if (m - mCount >= 0 && n - nCount >= 0) {
-            take = 1 + dfs(strs, m - mCount, n - nCount, i + 1);
+        if (m - counts[i][0] >= 0 && n - counts[i][1] >= 0) {
+            take = 1 + dfs(counts, m -counts[i][0] , n - counts[i][1], i + 1, memo);
         }
-        int skip = dfs(strs, m, n, i + 1);
+        int skip = dfs(counts, m, n, i + 1, memo);
         result = Math.max(take, skip);
+        memo[m][n][i] = result;
         return result;
     }
 }
